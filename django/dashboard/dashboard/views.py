7892d11b846ab43f5bddcce18de4a3ae03deb663
riwan321 <riwan.elansari@gmail.com>
2022-09-19 16:36:27 +0200

    doc pour utilisation des models

--- django/dashboard/dashboard/views.py
+++ django/dashboard/dashboard/views.py
@@ -8,5 +8,6 @@ def admin(request):
     return render(request, 'admin/admin.html')
 
 def test(request):
-    sensorList = sensor.objects.all()
-    return render (request, 'dashboard/test.html', {'sensorList':sensorList})
+    sensorList = sensor.objects.all() # Importe la liste des capteurs {'macAdress', 'naùe, 'interval'}
+    datas = datas.objects.all()  #Importe les datas brutes {'sensor', 't_stamp', 'value'}
+    return render (request, 'dashboard/test.html', {'sensorList':sensorList, 'datas' : datas})
